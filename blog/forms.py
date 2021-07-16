237cd76709c8c68d4dd9ffa9f2ed0769e56b4e65
Dan <misternutel@gmail.com>
2021-07-16 00:21:45 -0500

    now you can upload images with the form

--- blog/forms.py
+++ blog/forms.py
@@ -3,8 +3,6 @@ from django.db.models import fields
 from .models import Post
 
 
-# !if you try to upload an image in the form, it will not be read
-# FIXME make the form load the images
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
